7eb920294e219004cd4ffdaf3d735ac8c1dd2058
simv96 <17virdees@gmail.com>
2024-09-02 22:21:55 +0100

    recipe example added to js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ function generateRecipe(event) {
               100g/3½oz Parmesan, grated, plus extra for serving <br />
               1 tbsp full-fat milk (optional) <br />
               salt and freshly ground pepper <br /><br />
+              <strong>Method</strong> <br /><br />
+              1. Heat the oil in a frying pan over a medium heat. Add the
+              pancetta and cook until crisp and slightly browned. Remove the pan
+              from the heat and set aside. <br />
+              2. Bring a large saucepan of salted water to the boil. Add the
+              spaghetti and cook for 8 to 10 minutes, until firm but slightly
+              chewy (al dente). <br />
+              3. While the spaghetti is cooking, beat together the eggs and
+              Parmesan with a fork. If it's a little thick, add some milk.
+              Season well with salt and pepper. <br />
+              4. Once the spaghetti is cooked, drain over a bowl, reserving some
+              of the cooking water. Place the frying pan with the pancetta in
+              back over a medium to low heat. Add the drained spaghetti and egg
+              mixture and stir thoroughly so all the spaghetti is coated in the
+              sauce. Add 1 to 2 tablespoons of the reserved pasta water to
+              loosen, if necessary. Serve immediately on hot plates with grated
+              Parmesan on top.
              `,
     autoStart: true,
     delay: 1,
